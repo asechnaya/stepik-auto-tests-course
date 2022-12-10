ee750f2f4ed4a2a9b1d6b603880071a89ae544ea
Asja Makarowska(ya) <anutya@mail.ru>
2022-12-10 16:35:31 +0400

    add @pytest.mark.parametrize('language', ["ru", "en-gb"])

--- module 3/test_fixture7.py
+++ module 3/test_fixture7.py
@@ -17,3 +17,4 @@ def test_guest_should_see_login_link(browser, language):
     link = f"http://selenium1py.pythonanywhere.com/{language}/"
     browser.get(link)
     browser.find_element_by_css_selector("#login_link")
+    

957d4e0895e43b3cf70cc07a37fd1568582f16af
Asja Makarowska(ya) <anutya@mail.ru>
2022-12-10 16:33:15 +0400

    add multi destinations (browser links)

--- module 3/test_lesson36_step3.py
+++ module 3/test_lesson36_step3.py
@@ -6,13 +6,13 @@ from selenium import webdriver
 
 
 links_list = ['https://stepik.org/lesson/236895/step/1',
-'https://stepik.org/lesson/236896/step/1',
-'https://stepik.org/lesson/236897/step/1',
-'https://stepik.org/lesson/236898/step/1',
-'https://stepik.org/lesson/236899/step/1',
-'https://stepik.org/lesson/236903/step/1',
-'https://stepik.org/lesson/236904/step/1',
-'https://stepik.org/lesson/236905/step/1']
+              'https://stepik.org/lesson/236896/step/1',
+              'https://stepik.org/lesson/236897/step/1',
+              'https://stepik.org/lesson/236898/step/1',
+              'https://stepik.org/lesson/236899/step/1',
+              'https://stepik.org/lesson/236903/step/1',
+              'https://stepik.org/lesson/236904/step/1',
+              'https://stepik.org/lesson/236905/step/1']
 
 @pytest.fixture(scope="function")
 def browser():

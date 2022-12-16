ee07fa5d721cd4420fe964abadd56d762eaa49d3
Asja Makarowska(ya) <anutya@mail.ru>
2022-12-16 17:52:14 +0400

    driver to browser

--- void.py
+++ void.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.options import Options
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
-driver = webdriver.Chrome(options=chrome_options)
+browser = webdriver.Chrome(options=chrome_options)
 
 
 link = "http://suninjuly.github.io/simple_form_find_task.html"

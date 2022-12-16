0a623d7dd9c121c997561cf37fc8cd8b6ccaffc3
Asja Makarowska(ya) <anutya@mail.ru>
2022-12-16 15:52:22 +0400

    Update void.py

--- void.py
+++ void.py
@@ -3,21 +3,19 @@ import time
 
 link = "http://suninjuly.github.io/simple_form_find_task.html"
 
-try:
-    browser = webdriver.Chrome()
-    browser.get(link)
 
-    input1 = browser.find_element_by_tag_name(value1)
-    input1.send_keys("Ivan")
-    input2 = browser.find_element_by_name(value2)
-    input2.send_keys("Petrov")
-    input3 = browser.find_element_by_class_name(value3)
-    input3.send_keys("Smolensk")
-    input4 = browser.find_element_by_id(value4)
-    input4.send_keys("Russia")
-    button = browser.find_element_by_css_selector("button.btn")
-    button.click()
+browser = webdriver.Chrome()
+browser.get(link)
 
-finally:
-    time.sleep(30)
-    browser.quit()
+input1 = browser.find_element_by_tag_name(value1)
+input1.send_keys("Ivan")
+input2 = browser.find_element_by_name(value2)
+input2.send_keys("Petrov")
+input3 = browser.find_element_by_class_name(value3)
+input3.send_keys("Smolensk")
+input4 = browser.find_element_by_id(value4)
+input4.send_keys("Russia")
+button = browser.find_element_by_css_selector("button.btn")
+button.click()
+time.sleep(30)
+browser.quit()

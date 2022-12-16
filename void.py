217286fe33afe02b0a373e697e5e99bc17e41a1d
Asja Makarowska(ya) <anutya@mail.ru>
2022-12-16 15:56:40 +0400

    Update void.py

--- void.py
+++ void.py
@@ -1,10 +1,40 @@
 from selenium import webdriver
 import time 
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+import chromedriver_autoinstaller
+from pyvirtualdisplay import Display
+display = Display(visible=0, size=(800, 800))  
+display.start()
+
+chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
+                                      # and if it doesn't exist, download it automatically,
+                                      # then add chromedriver to path
+
+chrome_options = webdriver.ChromeOptions()    
+# Add your options as needed    
+options = [
+  # Define window size here
+   "--window-size=1200,1200",
+    "--ignore-certificate-errors"
+ 
+    #"--headless",
+    #"--disable-gpu",
+    #"--window-size=1920,1200",
+    #"--ignore-certificate-errors",
+    #"--disable-extensions",
+    #"--no-sandbox",
+    #"--disable-dev-shm-usage",
+    #'--remote-debugging-port=9222'
+]
+
+for option in options:
+    chrome_options.add_argument(option)
+
+    
+driver = webdriver.Chrome(options = chrome_options)
 
 link = "http://suninjuly.github.io/simple_form_find_task.html"
-
-
-browser = webdriver.Chrome()
 browser.get(link)
 
 input1 = browser.find_element_by_tag_name(value1)
